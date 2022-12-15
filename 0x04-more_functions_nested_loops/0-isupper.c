1ba229aac1dae2e30f1872913eb1e0d9d0e08f90
Richard A. Israel <richardakpabio0@gmail.com>
2022-12-15 08:03:15 -0800

    I supper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,16 +9,10 @@
 
 int _isupper(int c)
 {
-	char uppercase = 'A';
-	int isupper = 0;
-	for (; uppercase <= 'Z'; uppercase++)
+	if (c >= 65 && c <= 90)
 	{
-		if (c >=65 && c <= 91)
-		{
-			return (1);
-			break;
-		}
+	return (1);
 	}
-
+	else
 	return (0);
 }
